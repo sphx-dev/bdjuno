89c5e06e50f5a40b57dedf41655449d95aa311ef
Artem Linetskiy <artem.linetskiy@gmail.com>
2022-09-20 15:05:19 +0300

    Fix setup script && add push sha tag for docker image

--- database/schema/setup.sh
+++ database/schema/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -w -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/00-cosmos.sql
-echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -w -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/01-auth.sql
-echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -w -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/02-bank.sql
-echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -w -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/03-staking.sql
-echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -w -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/04-consensus.sql
-echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -w -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/05-mint.sql
-echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -w -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/06-distribution.sql
-echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -w -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/07-pricefeed.sql
-echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -w -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/09-modules.sql
-echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -w -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/10-slashing.sql
-echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -w -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/11-feegrant.sql
+echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -W -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/00-cosmos.sql
+echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -W -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/01-auth.sql
+echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -W -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/02-bank.sql
+echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -W -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/03-staking.sql
+echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -W -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/04-consensus.sql
+echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -W -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/05-mint.sql
+echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -W -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/06-distribution.sql
+echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -W -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/07-pricefeed.sql
+echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -W -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/09-modules.sql
+echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -W -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/10-slashing.sql
+echo $POSTGRES_PASSWORD | psql -h $POSTGRES_HOST -W -U $POSTGRES_USER -d $POSTGRES_DATABASE -f /var/lib/postgresql/schema/11-feegrant.sql
