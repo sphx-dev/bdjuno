ac01e32ba853ea82d0c9de20ca27c7a4d0f8a404
Wojtek <103407812+wojtek-coreum@users.noreply.github.com>
2024-02-22 12:45:30 +0100

    Remove `ensureRepo` function (#61)
    * Remove `ensureRepo` function
    
    * fix
    
    * fix

--- build/bdjuno/build.go
+++ build/bdjuno/build.go
@@ -5,14 +5,12 @@ import (
 	"path/filepath"
 
 	"github.com/CoreumFoundation/coreum-tools/pkg/build"
-	"github.com/CoreumFoundation/crust/build/git"
 	"github.com/CoreumFoundation/crust/build/golang"
 	"github.com/CoreumFoundation/crust/build/tools"
 )
 
 const (
-	repoURL    = "https://github.com/CoreumFoundation/bdjuno.git"
-	repoPath   = "../bdjuno"
+	repoPath   = "."
 	binaryName = "bdjuno"
 	binaryPath = "bin/" + binaryName
 )
@@ -33,15 +31,9 @@ func Test(ctx context.Context, deps build.DepsFunc) error {
 }
 
 func buildBDJuno(ctx context.Context, deps build.DepsFunc, targetPlatform tools.TargetPlatform) error {
-	deps(ensureRepo)
-
 	return golang.Build(ctx, deps, golang.BinaryBuildConfig{
 		TargetPlatform: targetPlatform,
 		PackagePath:    filepath.Join(repoPath, "cmd", "bdjuno"),
 		BinOutputPath:  filepath.Join("bin", ".cache", binaryName, targetPlatform.String(), "bin", binaryName),
 	})
-}
-
-func ensureRepo(ctx context.Context, deps build.DepsFunc) error {
-	return git.EnsureRepo(ctx, repoURL)
 }
