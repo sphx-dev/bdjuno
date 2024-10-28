8533d0bfff07f8acdffef80683b3e47ab7c1f6d7
augyg <galen.sprout@gmail.com>
2024-10-28 11:01:27 -0400

    fix position event types

--- modules/positions/handle_tx.go
+++ modules/positions/handle_tx.go
@@ -10,8 +10,13 @@ import (
 func (m *Module) HandleTx(tx *juno.Tx) error {
 	// Iterate through the events in the transaction directly
 	for _, event := range tx.Events {
+
+		/* EventTypeModifyPosition = "modify_position"
+
+			EventTypeNewPosition = "new_position"
+		) */
 		// Check if the event type matches the order events we're interested in
-		if event.Type == types.EventTypeMsgPlaceOrder || event.Type == types.EventTypeMsgCancelOrder {
+		if event.Type == types.EventTypeModifyPosition || event.Type == types.EventTypeNewPosition {
 			// Initialize a map to store the attributes of the event
 			attributes := make(map[string]string)
 			for _, attr := range event.Attributes {
